cdd011c208d3c1686f272f3a25daab09cd0fedd2
franko88bit <franks188@icloud.com>
2023-05-29 20:44:06 -0400

    created an array for users inputs

--- Develop/index.js
+++ Develop/index.js
@@ -6,8 +6,84 @@ const generateMarkdown = require("./utils/generateMarkdown.js");
 const api = require("./utils/api.js");
 
 // TODO: Create an array of questions for user input
-const questions = [
+const questions = [ 
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "username",
+        default: "Franko88Bit",
+        validate: function (answer) {
+            if (answer.length < 1 ) {
+                return console.log("A valid GitHub username is required.");
+            }
+            return true;
+        }
 
+    },
+    {
+        type: "input",
+        message: "What is the name of your GitHub repo?",
+        name: "repo",
+        default: "readme-generator",
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log('A valid GitHub username is required for a badge.');
+            }
+            return true;
+        }
+
+    },
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+        default: "Project Title",
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project title is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: "input",
+        message: "Write a description of your project?",
+        name: "Description",
+        default: "Project Description",
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project description is required.");
+
+            }
+            return true;
+        }
+    },
+    {
+        type: "input",
+        message: "If applicable, describe the steps required to install your project for the installation section.",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "provide instructions and examples of your project in use for the usage section.",
+        name:"usage"
+    },
+    {
+        type: "input",
+        message: "If applicable, provide any tests written for your application and provide examples on how to run them.",
+        name: "tests"
+    },
+    {
+        type: "list",
+        message: "if applicable, provide any tests written for your application and provide examples on how to run them.",
+        name: "tests"
+    },
+    {
+        type: "list",
+        message: "Choose a license for your project.",
+        choices: "GNU AGPAGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense']",
+        name: "license"
+    }
 ];
 
 // TODO: Create a function to write README file
